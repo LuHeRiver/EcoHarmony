fb57162a00eb729bc6745d8a1b633e3688004aa3
SarayAnta <s.rodriguezanta@gmail.com>
2024-01-16 09:09:03 +0100

    probando sonidos

--- src/js/piano.js
+++ src/js/piano.js
@@ -1,9 +1,33 @@
-const keys = document.querySelectorAll('.key');
+/* const keys = document.querySelectorAll('.key'); */
 
-keys.forEach(key =>{
+/* let keys = {
+    "1" : "../public/assets/audio/1.mp3",
+    "2" : "../public/assets/audio/2.mp3",
+    "3" : "../public/assets/audio/3.mp3",
+    "4" : "../public/assets/audio/4.mp3",
+    "5" : "../public/assets/audio/5.mp3",
+    "6" : "../public/assets/audio/6.mp3",
+    "7" : "../public/assets/audio/7.mp3",
+}
+ */
+
+
+/* keys.forEach(key =>{
     key.addEventListener('click', () => playNote(key))
 })
 function playNote(key){
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.play()
-}
+} */
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key =>{
+    key.addEventListener('click', () => {
+        let audio = new Audio("../public/assets/audio/1.mp3");
+        audio.play();
+    })
+});
+
+
+
